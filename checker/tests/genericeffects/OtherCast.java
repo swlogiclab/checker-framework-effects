0d36884926866a0ba894d7bc85bce307b0377e61
Rishi P <rishipatel1997@gmail.com>
2017-09-14 15:14:32 -0400

    Added manual and one extra test.
    Added manual for the casting effect checker and added test to make sure
    other casts do not get checked.

---
+++ checker/tests/genericeffects/OtherCast.java
@@ -0,0 +1,10 @@
+public class OtherCast {
+    public void testOtherCast() {
+        //Okay
+        String a = (String) "text";
+        //Okay
+        boolean b = (boolean) true;
+        //Okay
+        int c = (int) 'c';
+    }
+}
